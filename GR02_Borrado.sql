9bb3bfb217d8a1be67c2725587f1508b9c341917
santosluciano <luchosan74@gmail.com>
2018-06-25 19:31:19 -0300

    se modifico el borrado para que funciones en la base de datos provista por la catedra

--- GR02_Borrado.sql
+++ GR02_Borrado.sql
@@ -13,10 +13,10 @@ DROP TABLE IF EXISTS GR02_Tipo_Pago CASCADE;
 DROP TABLE IF EXISTS GR02_Estado_Luego_Ocupacion_Reserva CASCADE;
 DROP TABLE IF EXISTS GR02_Costo_Depto CASCADE;
 --- Borrado de funciones y funciones triggers
-DROP FUNCTION fn_gr02_departamento_estado;
-DROP FUNCTION fn_gr02_departamentos_disponibles;
-DROP FUNCTION trfn_gr02_cantidad_habitaciones;
-DROP FUNCTION trfn_gr02_cantidad_habitaciones_tipo;
-DROP FUNCTION trfn_gr02_cantidad_huespedes;
-DROP FUNCTION trfn_gr02_cantidad_huespedes_max;
-DROP FUNCTION trfn_gr02_reserva_huespedes_no_propietarios;
+DROP FUNCTION fn_gr02_departamento_estado(date);
+DROP FUNCTION fn_gr02_departamentos_disponibles(date,date,character varying);
+DROP FUNCTION trfn_gr02_cantidad_habitaciones();
+DROP FUNCTION trfn_gr02_cantidad_habitaciones_tipo();
+DROP FUNCTION trfn_gr02_cantidad_huespedes();
+DROP FUNCTION trfn_gr02_cantidad_huespedes_max();
+DROP FUNCTION trfn_gr02_reserva_huespedes_no_propietarios();
